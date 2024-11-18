11585bf20cef0b781ab1d3126820ccafa20bb7c0
Amour SODJINOU <amoursodjinou@gmail.com>
2024-11-18 15:19:29 +0100

    chore: stat

---
+++ src/app/models/statistic.model.ts
@@ -0,0 +1,20 @@
+export class Statistic {
+  id?: number;
+  created_at?: string;
+  updated_at?: string;
+  nbreAbonners?: number;
+  montantJournalierGlobals?: number;
+  montantJournalierDavier?: number;
+  montantJournalierVodougbe?: number;
+  montantJournalierAKEPDO?: number;
+  montantJournalierAKEP?: number;
+  montantMensuelsGlobals?: number;
+  montantAnnuelsGlobals?: number;
+  montantAnnes2020Globals?: number;
+  nombresRecharges?: number;
+  montantRecharges?: number;
+  nombresClient?: number;
+  montantJournalierGlobalsPrecedent?: number;
+  montantAnnuelsGlobalsPrecedent?: number;
+  poucentmontantAnnes2020GlobalsPrecedent?: number;
+}
