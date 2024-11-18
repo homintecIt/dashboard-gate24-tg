c57d36b00f7fbf22ecd38ff4b17cde133f42e0e5
stonefadel02 <afedjoustone@gmail.com>
2024-11-18 17:29:09 +0100

    update

---
+++ src/app/models/listeClient.model.ts
@@ -0,0 +1,12 @@
+
+export class ListeClientService {
+  id?: number;
+  created_at?: string;
+  updated_at?: string;
+  nom?: string;
+  prenom?: string;
+  email?: string;
+  tel?: number;
+  adresse?:string
+  cin?: string;
+}
