52a47123efb8055dbe5ea461f20bfb741402402f
stonefadel02 <afedjoustone@gmail.com>
2024-11-18 16:05:52 +0100

    update

---
+++ src/app/dashboard/components/liste-des-clients/liste-des-clients.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-des-clients',
+  templateUrl: './liste-des-clients.component.html',
+  styleUrls: ['./liste-des-clients.component.css']
+})
+export class ListeDesClientsComponent {
+
+}
---
+++ src/app/dashboard/components/liste-des-comptes-clients/liste-des-comptes-clients.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-des-comptes-clients',
+  templateUrl: './liste-des-comptes-clients.component.html',
+  styleUrls: ['./liste-des-comptes-clients.component.css']
+})
+export class ListeDesComptesClientsComponent {
+
+}
