fa9fedb08048ae00686cc506448d383f4e08d107
PierreGuede <pierrelarssonkhalifa@gmail.com>
2024-11-19 11:57:56 +0100

    commit about pagination and search

--- src/app/dashboard/components/recharges-list/recharges-list.component.ts
+++ src/app/dashboard/components/recharges-list/recharges-list.component.ts
@@ -1,43 +1,4 @@
-// import { Component, OnInit } from '@angular/core';
-// import { ApiService } from 'src/app/services/api.service';
-// import { Recharge } from '../../interfaces/recharges';
 
-// @Component({
-//   selector: 'app-recharges-list',
-//   templateUrl: './recharges-list.component.html',
-//   styleUrls: ['./recharges-list.component.css']
-// })
-// export class RechargesListComponent implements OnInit {
-//   recharges: Recharge[] = [];
-//   loading = false;
-//   error: string | null = null;
-
-//   constructor(private apiService: ApiService) {}
-
-//   ngOnInit() {
-//     this.fetchRecharges();
-//   }
-
-//   fetchRecharges() {
-//     this.loading = true;
-//     const payload = {
-
-//     };
-
-//     this.apiService.post<any>('/recharges/all', payload)
-//       .subscribe({
-//         next: (response) => {
-//           this.recharges = response.items;
-
-//           this.loading = false;
-//         },
-//         error: (err) => {
-//           console.error('Erreur de chargement', err);
-//           this.loading = false;
-//         }
-//       });
-//   }
-// }
 
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subject } from 'rxjs';
