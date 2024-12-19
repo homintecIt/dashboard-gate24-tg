2f82c9eac0c064000b45a323f54ec4dcf227371f
wilfried229 <wilfriedhoundjetin@gmail.com>
2024-12-19 23:39:06 +0100

    d

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
+
 export const environment = {
-  production: true,
-  apiUrl: 'http://peage.safer.tg:3000/api/v2/homintec',
-  // apiTestUrl: 'http://185.158.107.22:3000/api/v2/homintec'
-  apiTestUrl: 'http://185.158.107.22:3000/api/v2/homintec'
+  production: false,
+  apiUrl: 'http://10.13.40.42:3000/api/v2/homintec',
+  apiTestUrl: 'http://10.13.40.42:3000/api/v2/homintec'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
+
 export const environment = {
   production: false,
-  apiUrl: 'http://peage.safer.tg:3000/api/v2/homintec',
-  // apiTestUrl: 'https://role.ngrok.app/api/v2/homintec'
-  apiTestUrl: 'http://185.158.107.22:3000/api/v2/homintec'
+  apiUrl: 'http://10.13.40.42:3000/api/v2/homintec',
+  apiTestUrl: 'http://10.13.40.42:3000/api/v2/homintec'
 };
