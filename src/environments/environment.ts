a8ce9439169bb545c31c96a713667fc5024b4940
wilfried229 <wilfriedhoundjetin@gmail.com>
2025-08-16 17:42:47 +0100

    dd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
   // apiUrl: 'http://10.17.10.51:3000/api/v2/homintec',
   apiUrl: 'http://127.0.0.1:3000/api/v2/homintec',
   // apiTestUrl: 'http://10.17.10.51:3000/api/v2/homintec'
-  apiTestUrl: 'http://127.0.0.1:3000/api/v1/docs/swagger'
+  apiTestUrl: 'http://127.0.0.1:3000/api/v2/homintec'
 };
