03b6bb6a8fb6cffe387c355b37e8150436fc1281
wilfried229 <wilfriedhoundjetin@gmail.com>
2025-08-20 14:56:48 +0100

    update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://82.112.253.196:3000/api/v2/homintec',
-  // apiTestUrl: 'http://10.17.10.51:3000/api/v2/homintec'
   apiTestUrl: 'http://82.112.253.196:3000/api/v2/homintec'
+  
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,6 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'http://10.17.10.51:3000/api/v2/homintec',
   apiUrl: 'http://82.112.253.196:3000/api/v2/homintec',
-  // apiTestUrl: 'http://10.17.10.51:3000/api/v2/homintec'
   apiTestUrl: 'http://82.112.253.196:3000/api/v2/homintec'
 };
