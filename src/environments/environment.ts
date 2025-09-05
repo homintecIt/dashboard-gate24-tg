872c6e53e8d051c288077438b64be1a7d537a3b2
wilfried229 <wilfriedhoundjetin@gmail.com>
2025-09-05 12:45:22 +0100

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:3000/api/v2/homintec',
-  apiTestUrl: 'http://127.0.0.1:3000/api/v2/homintec'
+  apiUrl: 'http://10.13.40.42:3000/api/v2/homintec',
+  apiTestUrl: 'http://10.13.40.42:3000/api/v2/homintec'
 };
