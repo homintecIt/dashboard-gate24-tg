16c96e3f1163f4f544e4932c1353694d4fa285d9
wilfried229 <wilfriedhoundjetin@gmail.com>
2025-09-15 11:27:17 +0100

    update

--- src/app/dashboard/components/clients/edit-client-modal/edit-client-modal.component.ts
+++ src/app/dashboard/components/clients/edit-client-modal/edit-client-modal.component.ts
@@ -181,7 +181,7 @@ export class EditClientModalComponent implements OnInit {
 
   listAbonnementByCompte(data :any){
     this.generalService.successEvent.emit(data);
-    this.router.navigate(['/dashboard/subscribe-list/compte']);
+    this.router.navigate(['/dashboard/subscribe-list/compte',data.accountNumber]);
 
 
   }
