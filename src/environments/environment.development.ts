a799ba09c01d33d599892341dc7f98c128482c3d
wilfried229 <wilfriedhoundjetin@gmail.com>
2025-09-19 17:58:08 +0100

    dd

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://10.24.10.51:3000/api/v2/homintec',
-  apiTestUrl: 'http://127.0.0.1:3000/api/v2/homintec',
+  apiTestUrl: 'http://10.24.10.51:3000/api/v2/homintec',
   site:"AKEPEDO"
 
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://10.24.10.51:3000/api/v2/homintec',
-  apiTestUrl: 'http://127.0.0.1:3000/api/v2/homintec',
+  apiTestUrl: 'http://10.24.10.51:3000/api/v2/homintec',
   site:"AKEPEDO"
 };
