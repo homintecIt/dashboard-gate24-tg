20d9927d6b6ab2dbb30d8166f42b4acaca59030d
wilfried229 <wilfriedhoundjetin@gmail.com>
2025-09-19 17:57:30 +0100

    ss

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://10.17.10.51:3000/api/v2/homintec',
-  apiTestUrl: 'http://127.0.0.1:3000/api/v2/homintec',
+  apiTestUrl: 'http://10.17.10.51:3000/api/v2/homintec',
   site:"AKEPE"
 };
