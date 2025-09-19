46cf7d77c8db4bf51ac35a75ad118ca2327cf5a3
wilfried229 <wilfriedhoundjetin@gmail.com>
2025-09-19 17:43:10 +0100

    UPDATE

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://10.13.40.41:3000/api/v2/homintec',
+  apiUrl: 'http://10.17.10.51:3000/api/v2/homintec',
   apiTestUrl: 'http://127.0.0.1:3000/api/v2/homintec',
-  site:"DIRECTION"
+  site:"AKEPE"
 
 
 };
